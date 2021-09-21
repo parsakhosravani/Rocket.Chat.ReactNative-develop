ddcd87c67416be2afa14c70939e9a4e081f423a8
Parsa <hadi.khosravani@khanoumi.com>
2021-09-22 01:22:06 +0330

    first commit

---
+++ app/ee/omnichannel/selectors/inquiry.js
@@ -0,0 +1,5 @@
+import { createSelector } from 'reselect';
+
+const getInquiryQueue = state => state.inquiry.queued;
+
+export const getInquiryQueueSelector = createSelector([getInquiryQueue], queue => queue);
---
+++ app/lib/Icons.js
@@ -0,0 +1,7 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+
+import icoMoonConfig from './selection.json';
+
+const CustomIcon = createIconSetFromIcoMoon(icoMoonConfig, 'custom', 'custom.ttf');
+
+export { CustomIcon };
